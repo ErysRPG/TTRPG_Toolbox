210223d3f26e176400daf3b391e4d3998ca4fdec
ErysRPG <you@example.com>
2025-11-09 13:49:11 +0100

    Finished GUI

--- src/DiceRoller/DiceController.java
+++ src/DiceRoller/DiceController.java
@@ -1,5 +1,8 @@
 package DiceRoller;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -10,15 +13,37 @@ public class DiceController {
 
 	
 	@FXML 
-	Label rollResult;
+	Label rollResult, separatedDice;
 	@FXML
 	TextField diceNotationField;
 	@FXML
-	Button rollButton;
+	Button diceRollButton;
 	
 	public void roll() {
+		// get text from field
 		String diceNotation = diceNotationField.getText();
-		System.out.println(diceNotation);
-		rollResult.setText(diceNotation.toString());
+		
+		// split dice to separate size from quantity
+		String [] diceSplit = diceNotation.split("d");
+		String diceQuantity = diceSplit[0];
+		String diceSize = diceSplit[1];
+		
+		// create random number based on the size of the dice
+		Random rand = new Random();
+		int randomNumber = rand.nextInt(1, Integer.parseInt(diceSize) + 1);
+		
+		// Create an array to store the result of each dice
+		int diceResultArray[] = new int[Integer.parseInt(diceQuantity)];
+		
+		// for each dice in the array, choose a random number and set the labels
+		for (int dice : diceResultArray) {
+			int diceResult = Integer.parseInt(diceQuantity) * randomNumber;
+			diceResultArray[dice] = dice;
+			rollResult.setText(String.valueOf(diceResult));
+			separatedDice.setText(String.valueOf(Arrays.toString(diceResultArray)));
+		}
+		
+	
+		
 	}
 }
