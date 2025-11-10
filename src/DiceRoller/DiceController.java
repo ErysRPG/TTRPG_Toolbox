7df1f7d2706385ea7a071db30473a7bf35e44830
ErysRPG <you@example.com>
2025-11-10 13:31:58 +0100

    Started adding modifiers to dice

--- src/DiceRoller/DiceController.java
+++ src/DiceRoller/DiceController.java
@@ -2,6 +2,8 @@ package DiceRoller;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -11,53 +13,90 @@ import javafx.scene.control.TextField;
 
 public class DiceController {
 
-	
-	@FXML 
+	@FXML
 	Label rollResult, separatedDice;
 	@FXML
 	TextField diceNotationField;
 	@FXML
 	Button diceRollButton;
-	
-	public void roll() {
-		// get text from field
-		try {
-			String diceNotation = diceNotationField.getText();
-			// split dice to separate size from quantity
-			String [] diceSplit = diceNotation.split("d");
-			String diceQuantity = diceSplit[0];
-			String diceSize = diceSplit[1];
-			
-			// create random number based on the size of the dice
-			Random rand = new Random();
-			int randomNumber = rand.nextInt(1, Integer.parseInt(diceSize) + 1);
-			
-			// Create an array to store the result of each dice
-			int diceResultArray[] = new int[Integer.parseInt(diceQuantity)];
-			
-			// this variable will generate a random number rolling the dice
-			int randomResultInDice= Integer.parseInt(diceQuantity) * randomNumber;;
 
-			// generates a random number on the dice and
-			// stores the separate values of each dice's results
+	public void roll() {
+		try {
+
+			// get text from field
+			String diceNotation = diceNotationField.getText();
+			// Use a regex tp detect dice notation
+			Pattern p = Pattern.compile("([1-9]\\d*)?d([1-9]\\d*)([/x][1-9]\\d*)?([+-]\\d+)?");
+			Matcher m = p.matcher(diceNotation);
+
+			String diceQuantity = "";
+			String diceSize = "";
+			String diceSign = "";
+			String diceModifier = "";
 			
-			for (int i = 0; i < diceResultArray.length; i++) {
-				diceResultArray[i] = Integer.parseInt(diceQuantity) * randomNumber;
-				System.out.println(Arrays.toString(diceResultArray));
+			if (m.matches()) {
+				diceQuantity = m.group(1);
+				diceSize = m.group(2);
+				diceSign = m.group(3);
+				diceModifier = m.group(4);
+			} else {
+				rollResult.setText("Introduce dice notation");
 			}
 			
-			rollResult.setText(String.valueOf(randomResultInDice));
-			separatedDice.setText(String.valueOf(Arrays.toString(diceResultArray)));
 			
+
+			// Call Random to create a random number
+			Random rand = new Random();
+
+			// Create an array to store the result of each dice
+			int diceResultArray[] = new int[Integer.parseInt(diceQuantity)];
+
+			// this variable will generate a random number rolling the dice
+			int randomResultInDice = 0;
+
+			// this variable stores the total result
+			int total = 0;
+
+			for (int i = 0; i < Integer.parseInt(diceQuantity); i++) {
+				// generates a random number on the dice,
+				randomResultInDice = rand.nextInt(1, Integer.parseInt(diceSize) + 1);
+				// stores the separate values of each dice's results in an array
+				diceResultArray[i] = randomResultInDice;
+				// sums the result of the separate dice
+				total += randomResultInDice;
+			}
+			// stores the total result of the dice plus the operation corresponding to the
+			// modifier
+			int totalMod = 0;
+
+			switch (diceSign) {
+			case "+":
+				totalMod += total + Integer.parseInt(diceModifier);
+				rollResult.setText(String.valueOf(totalMod));
+				separatedDice.setText(String.valueOf(Arrays.toString(diceResultArray)));
+				break;
+			case "-":
+				totalMod += total - Integer.parseInt(diceModifier);
+				rollResult.setText(String.valueOf(totalMod));
+				separatedDice.setText(String.valueOf(Arrays.toString(diceResultArray)));
+				break;
+			case "*":
+				totalMod += total * Integer.parseInt(diceModifier);
+				rollResult.setText(String.valueOf(totalMod));
+				separatedDice.setText(String.valueOf(Arrays.toString(diceResultArray)));
+				break;
+			case "/":
+				totalMod += total / Integer.parseInt(diceModifier);
+				rollResult.setText(String.valueOf(totalMod));
+				separatedDice.setText(String.valueOf(Arrays.toString(diceResultArray)));
+				break;
+			default:
+				rollResult.setText(String.valueOf(total));
+				separatedDice.setText(String.valueOf(Arrays.toString(diceResultArray)));
+				break;
+			}
 		} catch (Exception e) {
-			rollResult.setText("Introduce dice notation");
+			rollResult.setText(e.toString());
 		}
-		
-		
-		
-			
-		}
-		
-	
-		
 	}
+}
--- src/UI/Main.java
+++ src/UI/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("mainView.fxml"));
 			Scene scene = new Scene(root);
+			primaryStage.setTitle("TTRPG Toolbox");
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
