cd6eb0e84788a04776547f0bf813cf1961eb5418
ErysRPG <you@example.com>
2025-11-17 17:14:27 +0100

    Started save and load data

--- src/SaveLoad/saveLoadController.java
+++ src/SaveLoad/saveLoadController.java
@@ -1,22 +1,33 @@
 package SaveLoad;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 
 public class saveLoadController {
 
 	@FXML
 	Button loadData, saveData;
+	@FXML
+	TextField fileName;
 	
-	public void saveData() {
+	public void saveData() throws IOException {
+		String name = fileName.getText();
+		BufferedWriter bw = new BufferedWriter(new FileWriter(name + ".txt"));
 		
-		FileChooser fc = new FileChooser();
-		fc.setTitle("Save file");
-		fc.showSaveDialog(null);
-		fc.setInitialFileName("Unnamed file");
-		fc.setSelectedExtensionFilter(null);
+	
+		
+	}
+	
+	public void LoadData() {
+		
 	}
 }
